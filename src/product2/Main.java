3a00764b559c3fb275cb2015d1feb737708bab5f
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-03-09 14:51:53 +0700

    finalClass

--- src/product2/Main.java
+++ src/product2/Main.java
@@ -6,13 +6,9 @@ public class Main {
         Produk p1 = new Produk("P01", "Laptop");
         Produk p2 = new Produk("P01", "Laptop");
 
-        if (p1.equals(p2)) {
-            System.out.println("Produk sama");
-        } else {
-            System.out.println("Produk berbeda");
-        }
-
-        String satu = "10";
-        System.out.println(satu.hashCode());
+        System.out.println(p1.equals(p2));
+        System.out.println(p1.hashCode() == p2.hashCode());
+        System.out.println(p1.hashCode());
+        System.out.println(p2.hashCode());
     }
 }
--- src/product2/Produk.java
+++ src/product2/Produk.java
@@ -10,17 +10,33 @@ public class Produk {
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((kodeProduk == null) ? 0 : kodeProduk.hashCode());
+        result = prime * result + ((namaProduk == null) ? 0 : namaProduk.hashCode());
+        return result;
+    }
+
+    @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
+        if (this == obj)
             return true;
-        }
-
-        if (obj == null || getClass() != obj.getClass()) {
+        if (obj == null)
             return false;
-        }
-
-        Produk p = (Produk) obj;
-
-        return this.kodeProduk.equals(p.kodeProduk) && this.namaProduk.equals(p.namaProduk);
+        if (getClass() != obj.getClass())
+            return false;
+        Produk other = (Produk) obj;
+        if (kodeProduk == null) {
+            if (other.kodeProduk != null)
+                return false;
+        } else if (!kodeProduk.equals(other.kodeProduk))
+            return false;
+        if (namaProduk == null) {
+            if (other.namaProduk != null)
+                return false;
+        } else if (!namaProduk.equals(other.namaProduk))
+            return false;
+        return true;
     }
 }
