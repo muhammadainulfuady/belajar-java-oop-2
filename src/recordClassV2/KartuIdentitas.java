4b8fa84249c5958c958176e62c75b3a427bed38b
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-03-14 21:09:04 +0700

    belajar record class

--- src/recordClassV2/KartuIdentitas.java
+++ src/recordClassV2/KartuIdentitas.java
@@ -2,13 +2,10 @@ package recordClassV2;
 
 public record KartuIdentitas(String nama, String nik, int umur, String kota) {
     public KartuIdentitas {
-        System.out.println("=== Kartu Identitas Digital ===");
+        System.out.println("\n=== Kartu Identitas Digital ===");
     }
 
-    public boolean isWargaDewasa() {
-        if (umur >= 18) {
-            return true;
-        }
-        return false;
+    public String isWargaDewasa() {
+        return umur >= 18 ? "Dewasa" : "Belum Dewasa";
     }
 }
--- src/recordClassV2/Main.java
+++ src/recordClassV2/Main.java
@@ -3,8 +3,17 @@ package recordClassV2;
 public class Main {
     public static void main(String[] args) {
         KartuIdentitas k1 = new KartuIdentitas("Budi Santoso", "3201234567890001", 25, "Bandung");
-        KartuIdentitas k2 = new KartuIdentitas("Ani Putri", "3201234567890002", 15, "Jakarta");
+        System.out.println("Nama    : " + k1.nama());
+        System.out.println("Nik     : " + k1.nik());
+        System.out.println("Umur    : " + k1.umur());
+        System.out.println("Kota    : " + k1.kota());
+        System.out.println("Status  : " + k1.isWargaDewasa());
 
-        k1.nama();
+        KartuIdentitas k2 = new KartuIdentitas("Ani Putri", "3201234567890002", 15, "Jakarta");
+        System.out.println("Nama    : " + k2.nama());
+        System.out.println("Nik     : " + k2.nik());
+        System.out.println("Umur    : " + k2.umur());
+        System.out.println("Kota    : " + k2.kota());
+        System.out.println("Status  : " + k2.isWargaDewasa());
     }
 }
